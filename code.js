505e54e0ab13c1f4f9a96c1ad4c3613c3f085b6a
Benjamin J Ferguson <ben.ferguson@duncanaviation.com>
2020-12-01 10:03:20 -0600

    test

--- code.js
+++ code.js
@@ -38,14 +38,72 @@ async function save(){
 }
 
 async function capture() {
+	setMode(MODE_CANVAS);
+
 	let canvas = document.getElementById('canvas');
 	let ctx = canvas.getContext('2d'); // get its context
 	let vid = document.querySelector('video');
 	// canvas.width = vid.videoWidth; // set its size to the one of the video
 	// canvas.height = vid.videoHeight;
-	ctx.drawImage(vid, 0,0); // the video
 
-	setMode(MODE_CANVAS);
+	// let url = 'https://www.rd.com/wp-content/uploads/2020/01/GettyImages-1131335393-e1580493890249-2048x1367.jpg';
+	// let image = new Image();
+	// image.src = url;
+	// image.onload = () => {
+	// 	// ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvas.width, canvas.height);
+	// 	drawImageProp(ctx, image, 0, 0, canvas.width, canvas.height);
+	// }
+
+	drawImageProp(ctx, vid, 0, 0, canvas.width, canvas.height);
+	// ctx.drawImage(vid, 0, 0); // the video
+}
+
+function drawImageProp(ctx, img, x, y, w, h, offsetX, offsetY) {
+
+    if (arguments.length === 2) {
+        x = y = 0;
+        w = ctx.canvas.width;
+        h = ctx.canvas.height;
+    }
+
+    // default offset is center
+    offsetX = typeof offsetX === "number" ? offsetX : 0.5;
+    offsetY = typeof offsetY === "number" ? offsetY : 0.5;
+
+    // keep bounds [0.0, 1.0]
+    if (offsetX < 0) offsetX = 0;
+    if (offsetY < 0) offsetY = 0;
+    if (offsetX > 1) offsetX = 1;
+    if (offsetY > 1) offsetY = 1;
+
+    var iw = img.width,
+        ih = img.height,
+        r = Math.min(w / iw, h / ih),
+        nw = iw * r,   // new prop. width
+        nh = ih * r,   // new prop. height
+        cx, cy, cw, ch, ar = 1;
+
+    // decide which gap to fill    
+    if (nw < w) ar = w / nw;                             
+    if (Math.abs(ar - 1) < 1e-14 && nh < h) ar = h / nh;  // updated
+    nw *= ar;
+    nh *= ar;
+
+    // calc source rectangle
+    cw = iw / (nw / w);
+    ch = ih / (nh / h);
+
+    cx = (iw - cw) * offsetX;
+    cy = (ih - ch) * offsetY;
+
+    // make sure source rectangle is valid
+    if (cx < 0) cx = 0;
+    if (cy < 0) cy = 0;
+    if (cw > iw) cw = iw;
+    if (ch > ih) ch = ih;
+
+    // fill image in dest. rectangle
+    ctx.drawImage(img, cx, cy, cw, ch,  x, y, w, h);
 }
 
 async function retake() {
